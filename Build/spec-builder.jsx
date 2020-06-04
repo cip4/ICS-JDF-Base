0a3477a4670622717c19fc1ae72335fa1a65de3a
Graham Mann <graham.mann@bilney.co.uk>
2020-06-04 19:50:30 +0100

    Test a standard FM build

---
+++ Build/spec-builder.jsx
@@ -0,0 +1,353 @@
+﻿#target framemaker
+
+/**
+ * This script creates PDF documents from Framemaker book.
+ * For the JDF/XJDF specification this requires application and selection of variables, formats etc
+ * to ensure the correct version is produced, ie the JDF 1.x, XJDF 2.x or Both. 
+ * The latter is the editing working version.
+ **/
+var TEMPLATE_JDF = "Template_V5_JDF1x.fm";
+var TEMPLATE_XJDF = "Template_V5_XJDF2x.fm";
+var TEMPLATE_BOTH = "Template_V5.fm";
+var TEMPLATE_PLAIN = "Template_V5.fm";
+var TEMPLATE_ICS = "Template_ICS.fm"
+var TEMPLATE_ICS_MIS = "Template_ICS_Mis.fm"
+var TEMPLATE_ICS_CUS = "Template_ICS_Cus.fm"
+
+var KEY_FILE_BOOK = "file.book";
+var KEY_FILE_LOG = "file.logging";
+var KEY_DIR_SOURCE = "dir.source";
+var KEY_DIR_TARGET = "dir.target";
+var KEY_BUILD_FILENAME = "build.filename";
+var KEY_BUILD_LAYOUT = "build.layout"
+
+// Configuration variables
+var COVER_TITLE = "doc.cover_title"
+var COVER_VERSION = "doc.cover_version"
+var COVER_BANNER_1 = "doc.cover_banner_1"
+var COVER_BANNER_2 = "doc.cover_banner_2"
+var RUNNING_TITLE = "doc.running_title"
+// And the variable names in all templates
+var COVER_TITLE_NAME = "Cover Title"
+var COVER_VERSION_NAME = "Cover Version"
+var COVER_BANNER_1_NAME = "Cover Banner 1"
+var COVER_BANNER_2_NAME = "Cover Banner 2"
+var RUNNING_TITLE_NAME = "Running Title"
+
+
+// !! ATTENTION !!
+// !! The following two lines are being overwritten by the python script. DON'T MODIFY !!
+log("---- Start Script [TEST-PROCESS] ----");
+var FILE_CONFIG = "C:\\Workspace\\spec-builder\\build.test.config";
+var PROCESS_ID = "TEST_PROCESS";
+
+log("JSX-Version (ExtendScript): 0.9");
+
+var filename = readConfig(KEY_FILE_BOOK);
+
+log("Load book '" + filename + "'...");
+var book = openFile(filename);
+
+log("Update components of book '" + book.Name + "'...");
+updateComponents(book);
+
+log("Update book '" + book.Name + "'...");
+updateBook(book);
+
+log("Save book '" + book.Name + "' as PDF...");
+savePdf(book);
+
+// log("Save book '" + book.Name + "' as HTML...");
+// saveHtml(book);
+
+log("Close book...")
+book.Close(Constants.FF_CLOSE_MODIFIED);
+
+log("Process completed.");
+$.sleep(5000)
+File(readConfig(KEY_DIR_TARGET) + PROCESS_ID).remove();
+
+
+/**
+ * Open the book or document.   
+ **/
+function openFile(filename) {  
+    
+    var openProps = GetOpenDefaultParams();
+    var i = GetPropIndex(openProps, Constants.FS_FileIsOldVersion);  
+    openProps[i].propVal.ival = Constants.FV_DoOK;  
+  
+    // open specification book.  
+    log("Open file '" + filename + "'...");
+    var file = Open(filename, openProps, new PropVals());  
+  
+    if (file.ObjectValid () === 1) {  
+        // Add a property to the document or book, indicating that the script opened it.  
+        file.openedByScript = true; 
+        log("File '" + filename + "' has been opened successfully.");
+    }  
+    else {  
+        // If the document can't be open, print the errors to the Console.  
+        log("Error during open file '" + filename + "'.");
+    }  
+  
+    return file; 
+} 
+
+/**
+ * Update all sections of a book.
+ **/
+function updateComponents(book) {
+    // load the one template required for this build layout
+    var layout = readConfig(KEY_BUILD_LAYOUT);
+    var template;
+    if(layout == "XJDF-BLACK") {
+        // XJDF 
+        template = openFile(readConfig(KEY_DIR_SOURCE) + TEMPLATE_XJDF);
+    } else if (layout == "JDF-BLACK") {
+        // JDF
+        template = openFile(readConfig(KEY_DIR_SOURCE) + TEMPLATE_JDF);
+    } else if (layout == "BOTH-COLORED") {
+        // Both
+        template = openFile(readConfig(KEY_DIR_SOURCE) + TEMPLATE_BOTH);
+    } else if (layout == "PLAIN") {
+        // Plain - i.e. unversioned
+        template = openFile(readConfig(KEY_DIR_SOURCE) + TEMPLATE_PLAIN);
+    } else if (layout == "ICS") {
+        // ICS - unversioned
+        template = openFile(readConfig(KEY_DIR_SOURCE) + TEMPLATE_ICS);
+    } else if (layout == "ICSMIS") {
+        // ICS - unversioned
+        template = openFile(readConfig(KEY_DIR_SOURCE) + TEMPLATE_ICS_MIS);
+    } else if (layout == "ICSCUS") {
+        // ICS - unversioned
+        template = openFile(readConfig(KEY_DIR_SOURCE) + TEMPLATE_ICS_CUS);
+    } else {
+      return; // No valid template
+    }
+    
+    log("Template used: " + template.Name);
+    // Change the variables in the template to reflect this build
+    updateTemplate(template);
+    
+    // Select which options of the template should be applied to all documents.
+    // For plain builds this is just the variables.
+    var formatFlags;
+    if (layout == "PLAIN") {
+        // Plain - i.e. unversioned
+        formatFlags = Constants.FF_UFF_VAR;     // Variables
+    } else {
+        // Versioned 
+        formatFlags = Constants.FF_UFF_COND	|	// Conditional Text Settings
+                      Constants.FF_UFF_FONT |   // Character Formats
+                      Constants.FF_UFF_VAR;     // Variables
+    }
+
+    // Update all documents in the book
+    // iterate over all book components
+    var component = book.FirstComponentInBook;    
+    while(component.id) {
+        // load document
+        file = openFile(component.Name);
+
+        log("Apply Template to '" + component.Name + "'");
+        file.SimpleImportFormats(template, formatFlags);
+    
+        // save and close 
+        file.Save(component.Name, GetSaveDefaultParams(), new PropVals());
+        log("Component '" + component.Name + "' has been saved.");
+        
+        file.Close(Constants.FF_CLOSE_MODIFIED);     
+        log("Component '" + component.Name + "' has been closed.");
+        
+        // next component
+        component = component.NextComponentInBook;
+    }
+
+    // Close the template - no longer required. Don't save any changes
+    template.Close(Constants.FF_CLOSE_MODIFIED);
+}
+
+/**
+ * Update references etc. in the book.
+ **/
+function updateBook(book) {
+	var params = GetUpdateBookDefaultParams();
+    var updateReturnParams = new PropVals();
+	
+	var i = GetPropIndex(params, Constants.FS_AlertUserAboutFailure);  
+    params[i].propVal.ival = true; 
+	
+	var i = GetPropIndex(params, Constants.FS_AllowInconsistentNumProps);  
+    params[i].propVal.ival = Constants.FV_DoOK;
+	
+	var i = GetPropIndex(params, Constants.FS_AllowNonFMFiles);  
+    params[i].propVal.ival = Constants.FV_DoShowDialog;
+	
+	var i = GetPropIndex(params, Constants.FS_AllowViewOnlyFiles);  
+    params[i].propVal.ival = Constants.FV_DoShowDialog;
+	
+	var i = GetPropIndex(params, Constants.FS_MakeVisible);  
+    params[i].propVal.ival = true;
+	
+	var i = GetPropIndex(params, Constants.FS_ShowBookErrorLog);  
+    params[i].propVal.ival = true;
+	
+	var i = GetPropIndex(params, Constants.FS_UpdateBookGeneratedFiles);  
+    params[i].propVal.ival = true;
+	
+	var i = GetPropIndex(params, Constants.FS_UpdateBookMasterPages);  
+    params[i].propVal.ival = false;
+	
+	var i = GetPropIndex(params, Constants.FS_UpdateBookNumbering);  
+    params[i].propVal.ival = true;
+	
+	var i = GetPropIndex(params, Constants.FS_UpdateBookOleLinks);  
+    params[i].propVal.ival = true;
+    
+    var i = GetPropIndex(params, Constants.FS_UpdateBookTextReferences);
+    params[i].propVal.ival = true;
+	
+    var i = GetPropIndex(params, Constants.FS_UpdateBookXRefs);
+    params[i].propVal.ival = true;
+
+	var returnValue = book.UpdateBook(params, updateReturnParams);
+    
+	// log values
+	log("Book update completed. ReturnValue: " + returnValue);
+	log("Number of ReturnParams: " + updateReturnParams.length)
+	
+	for(var n = 0; n < updateReturnParams.length; n ++) {
+		log("Update Result: " + updateReturnParams[n].propIdent.name + " - " + updateReturnParams[n].propVal.sval + "(" + updateReturnParams[n].propVal.ival + ")");
+	}
+}
+
+/**
+ * Modify the template.
+ */
+function updateTemplate(tpl) {
+    
+    // Change the template variables if the configuration file has new values
+    // Otherwise use the values from the template - default to adding the variables with an empty value
+    setVariable(tpl, COVER_TITLE_NAME, readConfig(COVER_TITLE));
+    setVariable(tpl, COVER_VERSION_NAME, readConfig(COVER_VERSION));
+    setVariable(tpl, COVER_BANNER_1_NAME, readConfig(COVER_BANNER_1));
+    setVariable(tpl, COVER_BANNER_2_NAME, readConfig(COVER_BANNER_2));
+    setVariable(tpl, RUNNING_TITLE_NAME, readConfig(RUNNING_TITLE));
+}
+
+/**
+ * Change or add a variable
+ **/
+function setVariable(doc, name, value) {
+    var varFmt = doc.GetNamedVarFmt(name);
+    if (varFmt.Name == null) {
+        // Create a new variable
+        varFmt = doc.NewNamedVarFmt(name);
+        varFmt.Fmt = "";
+    }
+    if (value != "") {
+        varFmt.Fmt = value;
+    }
+}
+
+/**
+ * Save as PDF.
+ **/
+function savePdf(book) {
+    var params = GetSaveDefaultParams();
+    var returnParamsp = new PropVals();
+
+    var i = GetPropIndex(params, Constants.FS_FileType);
+    params[i].propVal.ival = Constants.FV_SaveFmtPdf;
+
+    var path = readConfig(KEY_DIR_TARGET) + readConfig(KEY_BUILD_FILENAME) + ".pdf";
+    log("Write PDF to '" + path + "'");
+    book.Save(path, params, returnParamsp);
+}
+
+/**
+ * Save as HTML.
+ **/
+function saveHtml(book) {
+    var params = GetSaveDefaultParams();
+    var returnParamsp = new PropVals();
+
+    var i = GetPropIndex(params, Constants.FS_FileType);
+    params[i].propVal.ival = Constants.FV_SaveFmtFilter;
+    
+    var i = GetPropIndex(params, Constants.FS_SaveFileTypeHint);  
+    params[i].propVal.sval = "0001ADBEHTML";
+
+    var folderHtml = new Folder(readConfig(KEY_DIR_TARGET) + "\\html\\");  
+    folderHtml.create();
+
+    var path = readConfig(KEY_DIR_TARGET) + "html\\index.htm";
+    log("Write HTML files...");
+    book.Save(path, params, returnParamsp);
+}
+
+/**
+ * Read configuration by key.
+ **/
+function readConfig(key) {
+    
+    var result = undefined;
+    var file = File(FILE_CONFIG);
+    
+    file.open("r");
+    while (!file.eof) {
+        line = file.readln();
+        
+        if(line.indexOf(key + "=") == 0) {
+            result = line.split("=")[1];
+        }
+    }
+    file.close();
+
+    return result;
+}
+
+/**
+ * Log a message.
+ **/
+function log(message) {
+    var logFile = File(readConfig(KEY_FILE_LOG));
+    
+    // create timestamp string
+    var timestamp = "";
+    var now = new Date();
+    
+    timestamp += now.getFullYear() + "-";
+    timestamp += pad(now.getMonth() + 1, 2) + "-";
+    timestamp += pad(now.getDate(), 2);
+    timestamp += " ";
+    timestamp += pad(now.getHours(), 2) + ":";
+    timestamp += pad(now.getMinutes(), 2) + ":";
+    timestamp += pad(now.getSeconds(), 2) + ".";
+    timestamp += pad(now.getMilliseconds(), 3);
+    
+    // create line
+    var line = timestamp + " - " + message
+    
+    
+    // log line
+    logFile.open("a");
+    logFile.writeln(line);
+    logFile.close();
+}
+
+/**
+ * Fill up with leading zeros.
+ **/
+function pad(value, digits) {
+    var result = String(value) ;
+    
+    while(digits > result.length) {
+        result = '0' + result ;
+    }
+  
+    return result ;
+}
+
+
---
+++ Build/spec-builder.py
@@ -0,0 +1,221 @@
+import logging
+import os
+import shutil
+import sys
+import time
+from subprocess import call
+from subprocess import Popen
+from os.path import expanduser
+
+current_milli_time = lambda: int(round(time.time() * 1000))
+
+logging.basicConfig(level=logging.INFO)
+
+# constants
+FILE_RUNTIME_FM = "C:\Program Files (x86)\Adobe\AdobeFrameMaker2015\FrameMaker.exe"
+FILE_RUNTIME_ETSC = "C:\Program Files (x86)\Adobe\Adobe ExtendScript Toolkit CC\ExtendScript Toolkit.exe"
+FILE_SPEC_BUILDER_JSX = "spec-builder.jsx"
+FILE_BUILD_CONFIG = "build.config"
+FILE_LOG = "extend-script.log"
+FOLDER_TARGET = "target"
+PROCESS_ID = "p" + str(current_milli_time())
+
+BUILD_VERSION_DRAFT = ""
+
+# get params
+paramPathBook = sys.argv[1]  # path to book
+paramLayout = sys.argv[2]    # layout
+paramTitle = sys.argv[3]     # title
+paramVersion = sys.argv[4]   # version
+paramBuildNumber = ""        # build number
+paramGitVersion = ""         # git version
+
+if len(sys.argv) > 5:
+    paramBuildNumber = sys.argv[5]  # build number
+    paramGitVersion = sys.argv[6]   # git version
+
+    BUILD_VERSION_DRAFT = " build " + paramBuildNumber + " (rev: " + paramGitVersion[:11] + ")"
+
+# validate user input
+book = ""
+if os.path.exists(paramPathBook) and ".book" in paramPathBook:
+    book = paramPathBook
+else:
+    msg = "Parameter '" + paramPathBook + "' is not allowed."
+    logging.error(msg)
+    raise Exception(msg)
+
+if "XJDF-BLACK" != paramLayout and "JDF-BLACK" != paramLayout and "BOTH-COLORED" != paramLayout and "PLAIN" != paramLayout and "ICS" != paramLayout and "ICSMIS" != paramLayout and "ICSCUS" != paramLayout:
+    msg = "Invalid parameter '" + paramLayout + "'. Allowed values are 'XJDF-BLACK', 'JDF-BLACK', 'BOTH-COLORED', 'PLAIN', 'ICS', 'ICSMIS', 'ICSCUS'."
+    logging.error(msg)
+    raise Exception(msg)
+
+logging.info("Terminate running FrameMaker.exe if exists.")
+Popen("TASKKILL /F /IM FrameMaker.exe")
+time.sleep(5)
+
+BUILD_LAYOUT = paramLayout
+BUILD_VERSION = paramVersion.replace("$DATE", time.strftime('%Y%m%d')) + BUILD_VERSION_DRAFT
+DOC_TITLE = paramTitle
+BUILD_FILENAME = DOC_TITLE + " " + paramVersion.replace("$DATE", time.strftime('%Y%m%d'))
+
+if paramBuildNumber != '':
+    BUILD_FILENAME = BUILD_FILENAME + " build-" + paramBuildNumber
+
+logging.info("Start Framemaker...")
+FRAMEMAKER_PROCESS = Popen("\"" + FILE_RUNTIME_FM + "\"")
+
+PAUSE_PERIODE = 6
+logging.info("Wait for FrameMaker (%d sec.)..." % (PAUSE_PERIODE * 10))
+
+while PAUSE_PERIODE > 0:
+    time.sleep(10)
+    PAUSE_PERIODE -= 1
+    logging.info("%d secs..." % (PAUSE_PERIODE * 10))
+
+logging.info("Start processing '" + book + "' ...")
+logging.info("PROCESS_ID: " + PROCESS_ID)
+logging.info("BUILD_LAYOUT: " + BUILD_LAYOUT)
+logging.info("BUILD_FILENAME: " + BUILD_FILENAME)
+logging.info("DOC_TITLE: " + DOC_TITLE)
+
+
+#
+# Clean up the Framemaker Sources of the JDF Specification.
+#
+def cleanupSources(dir):
+    target = os.path.join(dir, FOLDER_TARGET)
+
+    if os.path.exists(target):
+        shutil.rmtree(target)
+
+    files = os.listdir(dir)
+
+    for f in files:
+        path = os.path.join(dir, f)
+
+        if ".backup.fm" in f:
+            os.remove(path)
+        elif ".recover.fm" in f:
+            os.remove(path)
+        elif ".recover-1.fm" in f:
+            os.remove(path)
+        elif ".backup.book" in f:
+            os.remove(path)
+        elif ".book." in f:
+            os.remove(path)
+        elif ".fm." in f:
+            os.remove(path)
+        elif ".fm.lck" in f:
+            os.remove(path)
+        elif " - Copy." in f:
+            os.remove(path)
+        elif "readme.doc" in f:
+            os.remove(path)
+        elif "build.config" in f:
+            os.remove(path)
+
+
+#
+# Create a build config file.
+#
+def createConfig(sourceDir, targetDir):
+    filename = os.path.join(targetDir, FILE_BUILD_CONFIG)
+
+    file = open(filename, "w")
+    file.write("build.filename=" + BUILD_FILENAME + "\n")
+    file.write("build.layout=" + BUILD_LAYOUT + "\n")
+    file.write("dir.source=" + sourceDir + "\\\n")
+    file.write("dir.target=" + targetDir + "\\\n")
+    file.write("file.book=" + book + "\n")
+    file.write("file.logging=" + os.path.join(targetDir, FILE_LOG) + "\n")
+    file.write("doc.title=" + DOC_TITLE + "\n")
+
+    file.write("doc.cover_title=" + DOC_TITLE + "\n")
+    file.write("doc.cover_version=" + BUILD_VERSION + "\n")
+    file.write("doc.running_title=" + DOC_TITLE + " " + BUILD_VERSION + "\n")
+    file.write("doc.cover_banner_1=" + "CoverBanner1" + "\n")
+    file.write("doc.cover_banner_2=" + "CoverBanner2" + "\n")
+
+    file.close()
+
+
+#
+# Run build process.
+#
+def runBuild(targetDir):
+    # get adobe trusted script folder
+    trustedScripts = os.path.join(expanduser("~"), "Documents\Adobe Scripts");
+
+    if not os.path.exists(trustedScripts):
+        os.makedirs(trustedScripts);
+        logging.warning("Trusted Scripts Directory hasn't exist.")
+
+    # deploy customized extend-script file to trusted location
+    src = os.path.join(os.path.dirname(os.path.realpath(__file__)), "extend-scripts", FILE_SPEC_BUILDER_JSX)
+    dest = os.path.join(trustedScripts, PROCESS_ID + "-" + FILE_SPEC_BUILDER_JSX)
+
+    fileSrc = open(src, "r")
+    fileDest = open(dest, "w")
+
+    for line in fileSrc:
+
+        if "[TEST-PROCESS]" in line:
+            line = line.replace("[TEST-PROCESS]", "(Process-ID: " + PROCESS_ID + ")")
+        elif "var PROCESS_ID =" in line:
+            line = "var PROCESS_ID = \"" + PROCESS_ID + "\";"
+        elif "var FILE_CONFIG =" in line:
+            dir = os.path.join(targetDir, FILE_BUILD_CONFIG).replace("\\", "\\\\")
+            line = "var FILE_CONFIG = \"" + dir + "\";"
+
+        fileDest.write(line)
+
+    fileSrc.close()
+    fileDest.close();
+
+    # prepare environment
+    open(os.path.join(targetDir, FILE_LOG), "a").close()
+
+    # run process
+    logging.info("Start FrameMaker ExtendScript...")
+    call([FILE_RUNTIME_ETSC, "-run", dest])
+
+
+rootDir = os.path.dirname(book)
+
+# cleanup directory
+cleanupSources(rootDir)
+
+# prepare build
+targetDir = os.path.join(rootDir, "target")
+os.makedirs(targetDir)
+createConfig(rootDir, targetDir)
+
+# start build
+runBuild(targetDir)
+
+# check running process
+processFile = os.path.join(targetDir, PROCESS_ID)
+logFile = open(os.path.join(targetDir, FILE_LOG), "r")
+open(processFile, "a").close()
+
+while os.path.isfile(processFile):
+    where = logFile.tell()
+    line = logFile.readline()
+
+    if not line:
+        time.sleep(1)
+        logFile.seek(where)
+    else:
+        msg = line.rstrip()
+        msg = msg[26:]
+        logging.info("ExtendScript: " + msg)
+
+logging.info("Close FrameMaker...")
+FRAMEMAKER_PROCESS.terminate()
+
+logging.info("-----------------------------")
+logging.info(" BUILD SUCCESSFUL")
+logging.info("-----------------------------")
+
+sys.exit()
